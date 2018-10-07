ac0b3cb50054e402d7bd06127cd231ec6d4d2e28
Raşit Özdemir <rasitozdemir@windowslive.com>
2018-10-07 13:09:18 +0300

    message is changed

--- scripts/poseBroadcaster.py
+++ scripts/poseBroadcaster.py
@@ -7,7 +7,7 @@ def talker():
     rospy.init_node('poseBroadcaster', anonymous=True)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
-        hello_str = "hello world foxy! %s" % rospy.get_time()
+        hello_str = "hello world mypc! %s" % rospy.get_time()
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
