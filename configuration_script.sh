eb85fd39c5166907e4de26b25998516960a6232b
Rasit Ozdemir <rasitozdemir@windowslive.com>
2018-10-13 21:09:38 +0300

    configuration script is coded

---
+++ configuration_script.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+YELLOW='\033[1;31;100m'
+RED='\033[1;31m'
+NC='\033[0m' # No Color
+ALERT='\033[1;32;104m'
+HEADER='\033[1;31;42m'
+printf "${RED}******************************************************************${NC}\n"
+printf "${RED}******************************************************************${NC}\n"
+printf "	 \n"
+printf "	 \n"
+printf "        Welcome to ${RED}foxy_swarm${NC} configuration script!\n"
+printf "	 \n"
+printf "	 \n"
+printf "${RED}******************************************************************${NC}\n"
+sudo echo "Please run \"ifconfig\" on another terminal and enter the name of wireless interface (wlan0, wlo1, etc)..."
+read interface_name
+echo ""
+printf "${RED}******************************************************************${NC}\n"
+sudo echo "Please select an IP for the current device..."
+read ip
+echo ""
+printf "${RED}******************************************************************${NC}\n"
+printf "${RED}******************************************************************${NC}\n"
+echo "Configuring /etc/network/interfaces..."
+echo "auto $interface_name" >> /etc/network/interfaces
+echo "iface $interface_name inet static" >> /etc/network/interfaces
+echo "    address $ip" >> /etc/network/interfaces
+echo "    netmask 255.255.255.0" >> /etc/network/interfaces
+echo "    wireless-channel 1" >> /etc/network/interfaces
+echo "    wireless-essid foxyAdHoc" >> /etc/network/interfaces
+echo "    wireless-mode ad-hoc" >> /etc/network/interfaces
+echo ""
+sudo echo "Ad-hoc network is configured with SSID foxyAdHoc."
+printf "${RED}******************************************************************${NC}\n"
+printf "${RED}******************************************************************${NC}\n"
+sudo apt-get install ros-$ROS_DISTRO-multimaster-fkie
+printf "${RED}******************************************************************${NC}\n"
+printf "${RED}******************************************************************${NC}\n"
+printf "foxy_swarm configurations are completed.\n"
+printf "			\n"
+printf "	${RED}foxySwarm${NC}: Ad-hoc networking project for swarm of drones\n"
+printf "		${HEADER}KOVAN Research Laboratory${NC} / ${HEADER}Middle East Technical University${NC} \n"
+printf "	 \n"
+printf "	${RED}Developer${NC}: \n"
+printf "		${HEADER}M. Rasit Ozdemir${NC}(M.Sc. Student)\n"
+printf "	\n"
+printf "	This project was done under the supervision of ${HEADER}Dr. Erol Şahin${NC} \n	with the infrastructural support of ${HEADER}KOVAN Research Laboratory${NC}.\n"
+printf "	Thanks to my friend ${HEADER}Burak Hocaoğlu${NC}(M.Sc. Student) for his support.\n"
+printf "	\n"
+printf "${RED}******************************************************************${NC}\n"
+printf "${RED}******************************************************************${NC}\n"
+echo "Press [ENTER] to reboot device or CTRL+C to exit without reboot. Notice"
+echo "that network changes will not be applied without reboot."
+read cnt
+sudo reboot
+
+
+
+
+
+
+
--- scripts/poseBroadcaster.py
+++ scripts/poseBroadcaster.py
@@ -4,22 +4,21 @@ from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped
 
 def talker():
-    pub = rospy.Publisher('poseBroadcaster', Pose, queue_size=2)
+    pub = rospy.Publisher('poseBroadcaster', PoseStamped, queue_size=2)
     rospy.init_node('poseBroadcaster', anonymous=True)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         current_pose = PoseStamped()
-
-		current_pose.header.frame_id = "rasp1"
-		current_pose.pose.position.x = 4.56123
-		current_pose.pose.position.y = 7.98327
-		current_pose.pose.position.z = 1.56445
-		current_pose.pose.orientation.x = 0.94988
-		current_pose.pose.orientation.y = 0.18811
-		current_pose.pose.orientation.z = 0.19811
-		current_pose.pose.orientation.w = 0.13872
+        current_pose.header.frame_id = "rasp1"
+        current_pose.pose.position.x = 4.56123
+        current_pose.pose.position.y = 7.98327
+        current_pose.pose.position.z = 1.56445
+        current_pose.pose.orientation.x = 0.94988
+        current_pose.pose.orientation.y = 0.18811
+        current_pose.pose.orientation.z = 0.19811
+        current_pose.pose.orientation.w = 0.13872
 		
-		rospy.loginfo(current_pose)
+        rospy.loginfo(current_pose)
         pub.publish(current_pose)
         rate.sleep()
 
