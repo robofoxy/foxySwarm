644d2df6331db1592f9035fd300cc23c974836d0
Rasit Ozdemir <rasitozdemir@windowslive.com>
2018-10-14 00:38:13 +0300

    some changes

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
-echo "Starting master_discovery..."
+echo "Running....."
+roscore >/dev/null 2>&1 &
 rosrun master_discovery_fkie master_discovery >/dev/null 2>&1 &
-sleep 5
-echo "Starting master_sync..."
-rosrun master_sync_fkie master_sync >/dev/null 2>&1
+rosrun master_sync_fkie master_sync >/dev/null 2>&1 &
 sleep 5
 echo ""
 echo "Available ROS topics:"
