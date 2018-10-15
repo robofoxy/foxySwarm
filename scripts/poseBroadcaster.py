3833e11891672e1cce0608e6efb96eac64c0f6df
Rasit Ozdemir <rasitozdemir@windowslive.com>
2018-10-15 22:02:46 +0300

    NETWORK MONITOR IS IMPLEMENTED

--- scripts/poseBroadcaster.py
+++ scripts/poseBroadcaster.py
@@ -1,30 +1,28 @@
 #!/usr/bin/env python
 import rospy
-import os
-from std_msgs.msg import String
+import os, random
 from geometry_msgs.msg import PoseStamped
 
-def talker():
+def pose_broadcaster():
     pub = rospy.Publisher('poseBroadcaster', PoseStamped, queue_size=2)
     rospy.init_node('poseBroadcaster', anonymous=True)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         current_pose = PoseStamped()
-        current_pose.header.frame_id = os.environ['ROS_HOSTNAME']
-        current_pose.pose.position.x = 4.56123
-        current_pose.pose.position.y = 7.98327
-        current_pose.pose.position.z = 1.56445
-        current_pose.pose.orientation.x = 0.94988
-        current_pose.pose.orientation.y = 0.18811
-        current_pose.pose.orientation.z = 0.19811
-        current_pose.pose.orientation.w = 0.13872
-		
+        current_pose.header.frame_id = os.environ['ROS_HOSTNAME'] #str(random.randint(0,2)) #SIM MODE
+        current_pose.pose.position.x = random.uniform(-4.5, 4.9)
+        current_pose.pose.position.y = random.uniform(-4.5, 4.9)
+        current_pose.pose.position.z = random.uniform(-4.5, 4.9)
+        current_pose.pose.orientation.x = random.uniform(-1.5, 1.9)
+        current_pose.pose.orientation.y = random.uniform(-1.5, 1.9)
+        current_pose.pose.orientation.z = random.uniform(-1.5, 1.9)
+        current_pose.pose.orientation.w = random.uniform(-1.5, 1.9)
         rospy.loginfo(current_pose)
         pub.publish(current_pose)
         rate.sleep()
 
 if __name__ == '__main__':
     try:
-        talker()
+        pose_broadcaster()
     except rospy.ROSInterruptException:
         pass
--- scripts/poseSubscriber.py
+++ scripts/poseSubscriber.py
@@ -1,23 +1,66 @@
 #!/usr/bin/env python
-import rospy
-from std_msgs.msg import String
+import rospy, sys, os, tf, curses, math
+from geometry_msgs.msg import PoseStamped
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + 'I heard "%s"', data.data)
+pose_dictionary = {}
 
-def listener():
+def pose_callback(poseStamped):
+	pose_dictionary[poseStamped.header.frame_id] = poseStamped
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('poseSubscriber', anonymous=True)
+def ln(num):
+	return len(str(num))
+	
+def toStr(num):
+	s = "%3.3f" % num
+	if num <0:
+		res =  " " + s
+	else:
+		res =  "  " + s
+	
+	return res + (11 - len(res)) * ' '
+	
+def print_pose(scr, key, i):
+	poseStamped = pose_dictionary[key]
+	frame = poseStamped.header.frame_id
+	px = poseStamped.pose.position.x
+	py = poseStamped.pose.position.y
+	pz = poseStamped.pose.position.z
+	quaternion = (poseStamped.pose.orientation.x, poseStamped.pose.orientation.y, poseStamped.pose.orientation.z, poseStamped.pose.orientation.w)
+	euler = tf.transformations.euler_from_quaternion(quaternion)
+	#ox = poseStamped.pose.orientation.x
+	#oy = poseStamped.pose.orientation.y
+	#oz = poseStamped.pose.orientation.z
+	#ow = poseStamped.pose.orientation.w
+	scr.addstr(i,0,"#|    {0}{1}||{2}|{3}|{4}|{5}|{6}|{7}|#".format(frame, ' '*(16-len(frame)), toStr(px), toStr(py), toStr(pz), toStr(euler[0]), toStr(euler[1]), toStr(euler[2])))
 
-    rospy.Subscriber('poseBroadcaster', String, callback)
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+def print_dict(scr):
+	scr.addstr(0,0,"#################################################################################################")
+	scr.addstr(1,0,"#############                           FOXY_SWARM NETWORK                          #############")
+	scr.addstr(2,0,"#################################################################################################")
+	scr.addstr(3,0,"#|    ROS_HOSTNAME    ||     X     |     Y     |     Z     |   ROLL    |   PITCH   |    YAW    |#")
+	scr.addstr(4,0,"#|---------------------------------------------------------------------------------------------|#")
+	if len(pose_dictionary) > 0:
+		i = 5
+		for key, poseStamped in pose_dictionary.items():
+			print_pose(scr, key, i)
+			i += 1
+		scr.addstr(i,0,"#|---------------------------------------------------------------------------------------------|#")
+		scr.addstr(i+1,0,"#################################################################################################")
+		scr.refresh()
+
+def pose_subscriber():
+	rospy.init_node('poseSubscriber', anonymous=True)
+	rospy.Subscriber('poseBroadcaster', PoseStamped, pose_callback)
+	scr = curses.initscr()
+	curses.noecho()
+	curses.cbreak()	
+	while not rospy.is_shutdown():
+		print_dict(scr)
+		rospy.sleep(0.4)
 
 if __name__ == '__main__':
-    listener()
+	try:
+		pose_subscriber()
+	except rospy.ROSInterruptException:
+		pass
