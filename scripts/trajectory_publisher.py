780f7bb05eaa04595d2f69abd30f4f85dd2fe567
Rasit Ozdemir <rasitozdemir@windowslive.com>
2018-11-11 12:39:22 +0300

    trajectory publishing for drones separately is completed

--- scripts/trajectory_publisher.py
+++ scripts/trajectory_publisher.py
@@ -2,7 +2,7 @@
 import rospy
 from multimaster_msgs_fkie.msg import LinkStatesStamped
 from multimaster_msgs_fkie.msg import LinkState
-from msg/foxySwarm.msg import TrajectoryPolynomialPiece
+from foxySwarm.msg import TrajectoryPolynomialPiece
 
 links = list()
 
@@ -15,19 +15,30 @@ def print_links():
 	for i in range(0, len(links)):
 		print links[i].destination
 	
+def create_fake_trj(dest):
+	msg = TrajectoryPolynomialPiece()
+	msg.destination = dest
+	for i in range(1000):
+		msg.poly_x.append(101)
+		msg.poly_y.append(102)
+		msg.poly_z.append(103)
+		msg.poly_yaw.append(0.5)
+		msg.duration = rospy.Duration(10, 0)
+	return msg
+	
 def RR():
 	rospy.init_node('linksSubscriber', anonymous=True)
 	rospy.Subscriber('/master_discovery/linkstats', LinkStatesStamped, links_callback)
-	rospy.Publisher('trajectoryPublisher', TrajectoryPolynomialPiece, queue_size=30)
+	trj_publisher = rospy.Publisher('trajectoryPublisher', TrajectoryPolynomialPiece, queue_size=30)
 	
 	while not rospy.is_shutdown():
+		for i in range(len(links)):
+			trj_publisher.publish(create_trj(links[i].destination))
 		print_links()
 		rospy.sleep(0.4)
-
 	
 if __name__ == '__main__':
 	try:
 		RR()
-		
 	except rospy.ROSInterruptException:
 		pass
