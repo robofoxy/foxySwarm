134f267df2f2820d3cdf4728ab6de788f7bddcad
Rasit Ozdemir <rasitozdemir@windowslive.com>
2018-11-11 10:08:28 +0300

    problem in exit script is fixed!

--- scripts/exit.sh
+++ scripts/exit.sh
@@ -1,6 +1,8 @@
 echo "Killing roscore and scripts..."
 sudo killall -9 roscore
+killall -9 rosmaster
 killall -9 master_discovery
 killall -9 master_sync
 echo ""
-echo "DONE"
+rostopic list
+echo "so, DONE"
