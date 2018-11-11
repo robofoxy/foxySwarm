9ec8223bc7f195176e1961e3ae1a43b1583425c8
Rasit Ozdemir <rasitozdemir@windowslive.com>
2018-11-11 11:57:25 +0300

    msg is taken from crazyflie_gazebo

--- scripts/trajectory_publisher.py
+++ scripts/trajectory_publisher.py
@@ -1,3 +1,33 @@
 #!/usr/bin/env python
 import rospy
-from 
+from multimaster_msgs_fkie.msg import LinkStatesStamped
+from multimaster_msgs_fkie.msg import LinkState
+from msg/foxySwarm.msg import TrajectoryPolynomialPiece
+
+links = list()
+
+def links_callback(link_states):
+	global links
+	links = link_states.links
+
+def print_links():
+	print "----------------------------"
+	for i in range(0, len(links)):
+		print links[i].destination
+	
+def RR():
+	rospy.init_node('linksSubscriber', anonymous=True)
+	rospy.Subscriber('/master_discovery/linkstats', LinkStatesStamped, links_callback)
+	rospy.Publisher('trajectoryPublisher', TrajectoryPolynomialPiece, queue_size=30)
+	
+	while not rospy.is_shutdown():
+		print_links()
+		rospy.sleep(0.4)
+
+	
+if __name__ == '__main__':
+	try:
+		RR()
+		
+	except rospy.ROSInterruptException:
+		pass
